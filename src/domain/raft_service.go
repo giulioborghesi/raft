d9a7fea23efed30a211d14bd1c8f2421d0066a43
Giulio Borghesi <giulio.borghesi.1981@gmail.com>
2021-01-12 06:39:39 -0800

    Add startElection method to raftServer (#10)

--- src/domain/raft_service.go
+++ src/domain/raft_service.go
@@ -4,8 +4,9 @@ import "sync"
 
 type RaftService struct {
 	sync.Mutex
-	state *serverState
-	roles map[int]serverRole
+	state         *serverState
+	remoteServers []string
+	roles         map[int]serverRole
 }
 
 // AppendEntry implements the AppendEntry RPC call. Firstly, RaftService will
@@ -20,13 +21,13 @@ func (s *RaftService) AppendEntry(remoteServerTerm int64,
 	defer s.Unlock()
 
 	// Only a follower can respond to an AppendEntry request
-	if ok := s.roles[state.role].makeFollower(state.currentTerm(),
+	if ok := s.roles[s.state.role].makeFollower(s.state.currentTerm(),
 		s.state); !ok {
 		return s.state.currentTerm(), false
 	}
 
 	// Append entry to log if possible
-	return s.roles[state.role].appendEntry(remoteServerTerm,
+	return s.roles[s.state.role].appendEntry(remoteServerTerm,
 		remoteServerID, s.state)
 }
 
@@ -43,12 +44,42 @@ func (s *RaftService) RequestVote(remoteServerTerm int64,
 	defer s.Unlock()
 
 	// Only a follower can grant its vote to a remote server
-	if ok := s.roles[state.role].makeFollower(state.currentTerm(),
+	if ok := s.roles[s.state.role].makeFollower(s.state.currentTerm(),
 		s.state); !ok {
 		return s.state.currentTerm(), false
 	}
 
 	// Grant vote if possible
-	return s.roles[state.role].requestVote(remoteServerTerm,
+	return s.roles[s.state.role].requestVote(remoteServerTerm,
 		remoteServerID, s.state)
+}
+
+func (s *RaftService) StartElection() {
+	// Lock access to server state
+	s.Lock()
+
+	// Only a candidate server can start an election
+	if ok := s.roles[s.state.role].makeCandidate(s.state); !ok {
+		s.Unlock()
+		return
+	}
+	electionTerm := s.state.currentTerm()
+
+	// Request votes asynchronously from remote servers
+	asyncResults := s.roles[s.state.role].startElection(s.remoteServers,
+		s.state)
+	s.Unlock()
+
+	// Wait for RPC calls to complete (or fail due to timeout)
+	results := make([]requestVoteResult, 0)
+	for _, asyncResult := range asyncResults {
+		results = append(results, <-asyncResult)
+	}
+
+	// Lock access to server state again
+	s.Lock()
+	defer s.Unlock()
+
+	// Finalize election
+	s.roles[s.state.role].finalizeElection(electionTerm, results, s.state)
 }
