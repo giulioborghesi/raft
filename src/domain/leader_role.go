9981f26cfa89f758ef7e8d813b5c70fe7b16f480
Giulio Borghesi <giulio.borghesi.1981@gmail.com>
2021-01-15 08:42:55 +0000

    Add unit test for leaderRole

--- src/domain/leader_role.go
+++ src/domain/leader_role.go
@@ -6,61 +6,64 @@ import (
 )
 
 const (
-	leaderErrFmt = "%s should not be called when a server is a follower"
+	leaderErrMultFmt = "multiple leaders in the same term detected"
 )
 
 type leaderRole struct{}
 
 func (l *leaderRole) appendEntry(serverTerm int64,
 	serverID int64, s *serverState) (int64, bool) {
-	// Get current term
-	currentTerm := s.currentTerm()
-
-	// No two leaders can be elected during the same term
-	if currentTerm == serverTerm {
-		panic("cannot have more than one leader per term")
-	}
-
-	// If calling server term is greater than local server term, role should
-	// have been changed to follower: failure to do so points to a bug
-	if currentTerm < serverTerm {
-		panic("leader has not transitioned to follower before" +
-			"calling appendEntry")
-	}
-	return currentTerm, false
+	panic(fmt.Sprintf(roleErrCallFmt, "appendEntry", "leader"))
 }
 
 func (l *leaderRole) finalizeElection(_ int64, _ []requestVoteResult,
 	_ *serverState) {
-	panic(fmt.Sprintf(leaderErrFmt, "finalizeElection"))
+	panic(fmt.Sprintf(roleErrCallFmt, "finalizeElection", "leader"))
 }
 
 func (l *leaderRole) makeCandidate(_ time.Duration, s *serverState) bool {
+	// Leaders cannot transition to candidates
 	return false
 }
 
-func (l *leaderRole) makeFollower(serverTerm int64, s *serverState) bool {
+func (l *leaderRole) prepareAppend(serverTerm int64, serverID int64,
+	s *serverState) bool {
 	// Get current term
 	currentTerm := s.currentTerm()
 
-	// Leader remains leader if server term not greater than current term
-	if currentTerm >= serverTerm {
+	if currentTerm == serverTerm {
+		// Multiple leaders in the same term are not allowed
+		panic(fmt.Sprintf(leaderErrMultFmt))
+	} else if currentTerm > serverTerm {
+		// Request received from a former leader, reject it
 		return false
 	}
 
-	// Change role to follower, update term and last modified
+	// Request received from new leader, transition to follower
 	s.role = follower
 	s.updateTerm(serverTerm)
+	s.leaderID = serverID
 	s.lastModified = time.Now()
 	return true
 }
 
 func (l *leaderRole) requestVote(serverTerm int64, serverID int64,
 	s *serverState) (int64, bool) {
-	panic(fmt.Sprintf(leaderErrFmt, "requestVote"))
+	// Get current term
+	currentTerm := s.currentTerm()
+	if currentTerm >= serverTerm {
+		return currentTerm, false
+	}
+
+	// Server term greater than local term, cast vote and become follower
+	s.role = follower
+	s.updateTermVotedFor(serverTerm, serverID)
+	s.leaderID = invalidLeaderID
+	s.lastModified = time.Now()
+	return serverTerm, true
 }
 
 func (l *leaderRole) startElection(servers []string,
 	s *serverState) []chan requestVoteResult {
-	panic(fmt.Sprintf(leaderErrFmt, "startElection"))
+	panic(fmt.Sprintf(roleErrCallFmt, "startElection", "leader"))
 }
