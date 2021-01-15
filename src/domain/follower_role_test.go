3e703c7e6e73639ec3b1b5e8a12f666e093ac67a
Giulio Borghesi <giulio.borghesi.1981@gmail.com>
2021-01-15 08:42:42 +0000

    Add unit test for followerRole

--- src/domain/follower_role_test.go
+++ src/domain/follower_role_test.go
@@ -1,189 +1,250 @@
 package domain
 
 import (
+	"fmt"
 	"testing"
 	"time"
+
+	server_state_dao "github.com/giulioborghesi/raft-implementation/src/datasources"
+	"github.com/giulioborghesi/raft-implementation/src/utils"
 )
 
 const (
-	startingTerm     = 15
-	startingVotedFor = 0
+	testFollowerStartingTerm = 15
+	testFollowerVotedFor     = 1
+	testFollowerLeaderID     = 2
+	testFollowerRemoteID     = 4
 )
 
-// mockedServerStateDao is a server state DAO mack
-type mockedServerStateDao struct {
-	currentTerm int64
-	votedFor    int64
-}
+// MakeFollowerServerState creates and initializes an instance of serverState
+// for a server in follower mode
+func MakeFollowerServerState() *serverState {
+	dao := server_state_dao.MakeTestServerStateDao()
+	dao.UpdateTerm(testFollowerStartingTerm)
+	dao.UpdateVotedFor(testFollowerVotedFor)
 
-func (s *mockedServerStateDao) CurrentTerm() int64 {
-	return s.currentTerm
-}
-
-func (s *mockedServerStateDao) UpdateTerm(serverTerm int64) error {
-	s.currentTerm = serverTerm
-	s.votedFor = -1
-	return nil
-}
-
-func (s *mockedServerStateDao) UpdateTermVotedFor(serverTerm int64,
-	serverID int64) error {
-	s.currentTerm = serverTerm
-	s.votedFor = serverID
-	return nil
-}
-
-func (s *mockedServerStateDao) UpdateVotedFor(serverID int64) error {
-	s.votedFor = serverID
-	return nil
-}
-
-func (s *mockedServerStateDao) VotedFor() (int64, int64) {
-	return s.currentTerm, s.votedFor
-}
-
-func TestAppendEntry(t *testing.T) {
-	// Initialize mocked server state dao
-	dao := new(mockedServerStateDao)
-	dao.currentTerm = startingTerm
-	dao.votedFor = startingVotedFor
-
-	// Initialize server state
 	s := new(serverState)
 	s.dao = dao
+	s.leaderID = testFollowerLeaderID
+	return s
+}
 
-	// Instantiate follower
+func TestFollowerMethodsThatPanic(t *testing.T) {
+	// Initialize server state and follower
 	f := new(followerRole)
+	s := new(serverState)
 
-	// Server term is less than current term, append entry should fail
-	var serverTerm int64 = 10
-	var serverID int64 = 1
-	term, ok := f.appendEntry(serverTerm, serverID, s)
-	if term != startingTerm {
-		t.Errorf("wrong term following appendEntry: expected %d, actual %d",
-			startingTerm, term)
+	// Test finalizeElection
+	finalizeElection := func() {
+		f.finalizeElection(0, []requestVoteResult{}, s)
 	}
+	utils.AssertPanic(t, "finalizeElection", finalizeElection)
 
-	if ok {
-		t.Errorf("appendEntry succeded although it was expected to fail")
+	// Test startElection
+	startElection := func() {
+		f.startElection([]string{}, s)
 	}
+	utils.AssertPanic(t, "startElection", startElection)
+}
 
-	// Server term is greater than current term, append entry will succeed
-	serverTerm = 18
-	term, ok = f.appendEntry(serverTerm, serverID, s)
-	if term != serverTerm {
+func TestFollowerAppendEntry(t *testing.T) {
+	// Create server state and follower instance
+	f := new(followerRole)
+	s := MakeFollowerServerState()
+
+	// Call toppendEntry should succeed
+	serverTerm := s.currentTerm()
+	actualterm, ok := f.appendEntry(serverTerm, s.leaderID, s)
+	if actualterm != serverTerm {
 		t.Errorf("wrong term following appendEntry: expected %d, actual %d",
-			serverTerm, term)
+			serverTerm, actualterm)
 	}
 
 	if !ok {
-		t.Errorf("appendEntry failed although it was expected to succeed")
+		t.Errorf("appendEntry was not expected to fail")
 	}
+
+	// Server term different from local value, appendEntry should panic
+	appendEntryInvalidTerm := func() {
+		f.appendEntry(serverTerm+1, s.leaderID, s)
+	}
+	utils.AssertPanic(t, "appendEntry", appendEntryInvalidTerm)
+
+	// Leader ID different from local value, appendEntry should panic
+	appendEntryInvalidLeaderID := func() {
+		f.appendEntry(serverTerm, s.leaderID+1, s)
+	}
+	utils.AssertPanic(t, "appendEntry", appendEntryInvalidLeaderID)
 }
 
-func TestMakeCandidate(t *testing.T) {
-	// Initialize mocked server state dao
-	dao := new(mockedServerStateDao)
-	dao.currentTerm = startingTerm
-	dao.votedFor = startingVotedFor
-
-	// Initialize server state
-	s := new(serverState)
-	s.dao = dao
-	s.lastModified = time.Now()
-	s.serverID = 2
-
-	// Instantiate follower
+func TestFollowerMakeCandidate(t *testing.T) {
+	// Create server state and follower instance
 	f := new(followerRole)
+	s := MakeFollowerServerState()
 
-	// Change server role to candidate
-	f.makeCandidate(time.Duration(0), s)
-	term, votedFor := s.votedFor()
-	if term != startingTerm+1 {
-		t.Errorf("wrong term following makeCandidate: expected %d, actual %d",
-			startingTerm+1, term)
+	// Store initial term for later comparison
+	initialTerm := s.currentTerm()
+
+	// Initialize time and duration
+	s.lastModified = time.Now()
+	to := time.Second
+	success := f.makeCandidate(to, s)
+
+	// Apart from patological cases during test execution, this should fail
+	if success {
+		t.Fatalf("makeCandidate was expected to fail")
 	}
 
-	if votedFor != 2 {
-		t.Errorf("vote should have been casted for local server")
+	if s.currentTerm() != initialTerm {
+		t.Fatalf("makeCandidate should not change the current term")
+	}
+
+	if s.role != follower {
+		t.Fatalf(fmt.Sprintf("invalid server role following makeCandidate: "+
+			"expected: %d, actual: %d", follower, s.role))
+	}
+
+	// Ensure time elapsed exceeds timeout
+	s.lastModified = time.Now().Add(-to)
+	success = f.makeCandidate(to, s)
+
+	if !success {
+		t.Fatalf("makeCandidate was expected to succeed")
 	}
 
 	if s.role != candidate {
-		t.Errorf("role should be candidate following makeCandidate")
+		t.Fatalf(fmt.Sprintf("invalid server role following makeCandidate: "+
+			"expected: %d, actual: %d", candidate, s.role))
 	}
 }
 
-func TestRequestVote(t *testing.T) {
-	// Initialize mocked server state dao
-	dao := new(mockedServerStateDao)
-	dao.currentTerm = startingTerm
-	dao.votedFor = startingVotedFor
-
-	// Initialize server state
-	s := new(serverState)
-	s.dao = dao
-
-	// Instantiate follower
+func TestFollowerPrepareAppend(t *testing.T) {
+	// Create server state and follower instance
 	f := new(followerRole)
+	s := MakeFollowerServerState()
 
-	// Server term is less than current term, request vote will fail
-	var serverTerm int64 = startingTerm - 2
-	var serverID int64 = 1
-	term, ok := f.requestVote(serverTerm, serverID, s)
-	if term != startingTerm {
-		t.Errorf("wrong term following requestVote: expected %d, actual %d",
-			startingTerm, term)
-	}
-
-	if ok {
-		t.Errorf("requestVote succeded although it was expected to fail")
-	}
-
-	// Server term is equal to current term but vote was already casted
-	serverTerm = startingTerm
-	term, ok = f.requestVote(serverTerm, serverID, s)
-	if term != startingTerm {
-		t.Errorf("wrong term following requestVote: expected %d, actual %d",
-			startingTerm, term)
-	}
-
-	if ok {
-		t.Errorf("requestVote succeded although it was expected to fail")
-	}
-
-	// Term is now updated and votedFor is reset to nil
-	serverTerm = startingTerm + 5
-	s.updateTerm(serverTerm)
-	term, votedFor := s.votedFor()
-	if term != serverTerm {
-		t.Errorf("wrong term following updateTerm: expected %d, actual %d",
-			serverTerm, term)
-	}
-
-	if votedFor != -1 {
-		t.Errorf("votedFor was not reset following updateTerm")
-	}
-
-	// requestVote should now succeed
-	term, ok = f.requestVote(serverTerm, serverID, s)
-	if term != serverTerm {
-		t.Errorf("wrong term returned by requestVote: expected %d, actual %d",
-			serverTerm, term)
-	}
-
+	// Call to prepareAppend expected to succeed
+	serverTerm := s.currentTerm()
+	ok := f.prepareAppend(serverTerm, s.leaderID, s)
 	if !ok {
-		t.Errorf("requestVote failed although it was expected to succeed")
+		t.Fatalf("prepareAppend expected to succeed")
 	}
 
-	// If server term is greater than current term, requestVote should succeed
-	serverTerm += 2
-	term, ok = f.requestVote(serverTerm, serverID, s)
-	if term != serverTerm {
-		t.Errorf("wrong term returned by requestVote: expected %d, actual %d",
-			serverTerm, term)
+	if s.currentTerm() != testFollowerStartingTerm {
+		t.Fatalf("prepareAppend should not change the current term")
 	}
 
+	// Server term less than local term, prepareAppend expected to fail
+	serverTerm = s.currentTerm() - 1
+	ok = f.prepareAppend(serverTerm, testFollowerRemoteID, s)
+	if ok {
+		t.Fatalf("prepareAppend expected to fail")
+	}
+
+	if s.currentTerm() != testFollowerStartingTerm {
+		t.Fatalf("prepareAppend should not change the current term")
+	}
+
+	// Server term exceeds local term, prepareAppend will succeed and leader
+	// ID will be updated
+	serverTerm = s.currentTerm() + 1
+	ok = f.prepareAppend(serverTerm, testFollowerRemoteID, s)
 	if !ok {
-		t.Errorf("requestVote failed although it was expected to succeed")
+		t.Fatalf("prepareAppend expected to succeed")
+	}
+
+	if s.currentTerm() != serverTerm {
+		t.Fatalf(fmt.Sprintf("invalid term after prepareAppend: "+
+			"expected: %d, actual: %d", serverTerm, s.currentTerm()))
+	}
+
+	if s.leaderID != testFollowerRemoteID {
+		t.Fatalf("leader ID expected to change")
+	}
+
+	// Cause a panic as result of multiple leaders present in the same term
+	prepareAppend := func() {
+		serverID := int64(testFollowerRemoteID - 1)
+		f.prepareAppend(serverTerm, serverID, s)
+	}
+	utils.AssertPanic(t, "prepareAppend", prepareAppend)
+}
+
+func TestFollowerRequestVote(t *testing.T) {
+	// Create server state and follower instance
+	f := new(followerRole)
+	s := MakeFollowerServerState()
+
+	// Store initial term for further comparison
+	initialTerm := s.currentTerm()
+
+	// Already voted, return false
+	serverTerm := initialTerm
+	currentTerm, voted := f.requestVote(serverTerm, testFollowerRemoteID, s)
+
+	if currentTerm != initialTerm {
+		t.Fatalf("invalid term returned by requestVote: "+
+			"expected: %d, actual: %d", initialTerm, currentTerm)
+	}
+
+	if s.currentTerm() != initialTerm {
+		t.Fatalf("currentTerm not expected to change")
+	}
+
+	if voted {
+		t.Fatalf("requestVote not expected to grant a vote")
+	}
+
+	if s.leaderID != testFollowerLeaderID {
+		t.Fatalf("invalid leader ID following requestVote: "+
+			"expected: %d, actual: %d", testFollowerLeaderID, s.leaderID)
+	}
+
+	// Server did not vote yet, grant vote and return true
+	s.updateVotedFor(invalidLeaderID)
+	currentTerm, voted = f.requestVote(serverTerm, testFollowerRemoteID, s)
+
+	if s.currentTerm() != initialTerm && currentTerm != initialTerm {
+		t.Fatalf("invalid term returned by requestVote: "+
+			"expected: %d, actual: %d", initialTerm, currentTerm)
+	}
+
+	if !voted {
+		t.Fatalf("requestVote expected to grant a vote")
+	}
+
+	_, votedForID := s.votedFor()
+	if votedForID != testFollowerRemoteID {
+		t.Fatalf("votedFor invalid following requestVote: "+
+			"expected: %d, actual: %d", testFollowerLeaderID, votedForID)
+	}
+
+	if s.leaderID != testFollowerLeaderID {
+		t.Fatalf("invalid leader ID following requestVote: "+
+			"expected: %d, actual: %d", testFollowerLeaderID, s.leaderID)
+	}
+
+	// Server term exceeds local term, vote will be granted
+	serverTerm += 1
+	s.updateVotedFor(invalidLeaderID)
+	currentTerm, voted = f.requestVote(serverTerm, testFollowerRemoteID, s)
+	if s.currentTerm() != serverTerm && currentTerm != serverTerm {
+		t.Fatalf("invalid term returned by requestVote: "+
+			"expected: %d, actual: %d", initialTerm, currentTerm)
+	}
+
+	if !voted {
+		t.Fatalf("requestVote expected to grant a vote")
+	}
+
+	_, votedForID = s.votedFor()
+	if votedForID != testFollowerRemoteID {
+		t.Fatalf("votedFor invalid following requestVote: "+
+			"expected: %d, actual: %d", testFollowerLeaderID, votedForID)
+	}
+
+	if s.leaderID != invalidLeaderID {
+		t.Fatalf("invalid leader ID following requestVote: "+
+			"expected: %d, actual: %d", invalidLeaderID, s.leaderID)
 	}
 }
